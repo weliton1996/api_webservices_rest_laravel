0ca85decff4590ca1aca8f5d1f468c016544f19b
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-06-11 21:28:09 -0300

    first commit

---
+++ database/migrations/2024_06_05_005050_create_modelos_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modelos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('marca_id');
+            $table->string('nome',30);
+            $table->string('imagem',30);
+            $table->integer('numero_portas');
+            $table->integer('lugares');
+            $table->boolean('air_bag');
+            $table->boolean('abs');
+            $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('marca_id')->references('id')->on('marcas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Remove the foreign key constraint first
+        Schema::table('modelos', function (Blueprint $table) {
+            $table->dropForeign(['marca_id']);
+        });
+
+        // Then drop the table
+        Schema::dropIfExists('modelos');
+    }
+}
---
+++ database/migrations/2024_06_05_005201_create_carros_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('placa', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
+            $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('modelo_id')->references('id')->on('modelos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Remove the foreign key constraint first
+        Schema::table('carros', function (Blueprint $table) {
+            $table->dropForeign(['modelo_id']);
+        });
+
+        // Then drop the table
+        Schema::dropIfExists('carros');
+    }
+}
