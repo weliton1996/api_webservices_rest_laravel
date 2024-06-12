da872db5717c871106a9966fd24f26e1c00b2933
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-06-11 21:57:01 -0300

    A diferenca ente o metodo resource e apiRsouce, implementa outras rotas

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', function () {
-    return ['Chegamos até aqui' => 'Sim'];
-});
+//Método resource cria os métodos index, store, create, show, update, destroy e edit
+// Route::resource('cliente', 'App\http\Controllers\ClienteController');
+
+//Método resource cria somente os métodos index, store, show, update e destroy
+Route::apiResource('cliente', 'App\http\Controllers\ClienteController');
+
+Route::apiResource('carro', 'App\http\Controllers\CarroController');
+Route::apiResource('locacao', 'App\http\Controllers\LocacaoController');
+Route::apiResource('marca', 'App\http\Controllers\MarcaController');
+Route::apiResource('Modelo', 'App\http\Controllers\ModeloController');
+
+
