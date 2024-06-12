f24da06e1a579cc230b1323973d4b75b71709c05
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-06-11 22:01:09 -0300

    FIX: corrige namespace dos controller nas rotas

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::resource('cliente', 'App\http\Controllers\ClienteController');
 
 //Método resource cria somente os métodos index, store, show, update e destroy
-Route::apiResource('cliente', 'App\http\Controllers\ClienteController');
+Route::apiResource('cliente', 'ClienteController');
 
-Route::apiResource('carro', 'App\http\Controllers\CarroController');
-Route::apiResource('locacao', 'App\http\Controllers\LocacaoController');
-Route::apiResource('marca', 'App\http\Controllers\MarcaController');
-Route::apiResource('Modelo', 'App\http\Controllers\ModeloController');
+Route::apiResource('carro', 'CarroController');
+Route::apiResource('locacao', 'LocacaoController');
+Route::apiResource('marca', 'MarcaController');
+Route::apiResource('Modelo', 'ModeloController');
 
 
