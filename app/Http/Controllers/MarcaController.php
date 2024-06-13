1718180796c225750bf174522663f4ebe9139ce9
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-06-12 23:43:55 -0300

    Atualiza registro via put e path

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -19,16 +19,6 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -54,17 +44,6 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Marca $marca)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -73,7 +52,12 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        // print_r($request->all());//os dados atualizados
+        // echo '<hr>';
+        // print_r($marca->getAttributes());//os dados antigos
+
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
