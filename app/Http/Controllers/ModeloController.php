22d42a7cb1b1b8396d8ce4ede2a3bd65038ec531
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-07-20 18:30:42 -0300

    TEST: Testando os endpoints de modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -41,14 +41,13 @@ class ModeloController extends Controller
         $request->validate($this->modelo->rules(),$this->modelo->messages());
 
         $imagem = $request->file('imagem');
-        // $imagem->store('path', local|public);
         $imagem_urn = $imagem->store('imagens/modelos','public');
 
         $modelo = $this->modelo->create([
             'marca_id' =>  $request->marca_id,
             'nome' =>  $request->nome,
             'imagem' => $imagem_urn,
-            'numro_portas' => $request->numro_portas,
+            'numero_portas' => $request->numero_portas,
             'lugares' => $request->lugares,
             'air_bag'=> $request->air_bag,
             'abs'=> $request->abs,
@@ -82,8 +81,7 @@ class ModeloController extends Controller
     public function update(Request $request, $id)
     {
         $modelo = $this->modelo->find($id);
-        // dd($request->nome);
-        // dd($request->file('imagem'));
+
         if($modelo === null){
             return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe.'],404);
         }
@@ -113,11 +111,16 @@ class ModeloController extends Controller
         }
 
         $imagem = $request->file('imagem');
-        $imagem_urn = $imagem->store('imagens','public');
+        $imagem_urn = $imagem->store('imagens/modelos','public');
 
         $modelo->update([
+            'marca_id' => $request->marca_id,
             'nome' =>  $request->nome,
-            'imagem' => $imagem_urn
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag'=> $request->air_bag,
+            'abs'=> $request->abs,
         ]);
         return response()->json($modelo,200);
     }
