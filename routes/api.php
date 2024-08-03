0c16cdf26923584b2e9c316103a41972ed4b4875
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-08-03 15:54:42 -0300

    Criando um grupo de rotas protegidas com um prefix de versionamento

--- routes/api.php
+++ routes/api.php
@@ -21,16 +21,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Método resource cria os métodos index, store, create, show, update, destroy e edit
 // Route::resource('cliente', 'App\http\Controllers\ClienteController');
 
-//Método resource cria somente os métodos index, store, show, update e destroy
-Route::apiResource('cliente', 'ClienteController')->middleware('jwt.auth');
-Route::apiResource('carro', 'CarroController')->middleware('jwt.auth');
-Route::apiResource('locacao', 'LocacaoController')->middleware('jwt.auth');
-Route::apiResource('marca', 'MarcaController')->middleware('jwt.auth');
-Route::apiResource('modelo', 'ModeloController')->middleware('jwt.auth');
-
+Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    //Método resource cria somente os métodos index, store, show, update e destroy
+    Route::apiResource('cliente', 'ClienteController');
+    Route::apiResource('carro', 'CarroController');
+    Route::apiResource('locacao', 'LocacaoController');
+    Route::apiResource('marca', 'MarcaController');
+    Route::apiResource('modelo', 'ModeloController');
+});
 Route::post('login', 'AuthController@login');
 Route::post('logout', 'AuthController@logout');
 Route::post('refresh', 'AuthController@refresh');
 Route::post('me', 'AuthController@me');
+
 
 
