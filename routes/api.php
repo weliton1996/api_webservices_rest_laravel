dbda17092ff6ab1b4e3e8009ff662f993508afd5
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-08-03 14:56:31 -0300

    Criando as rotas de autenticação e autorização e o AuthController

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,9 @@ Route::apiResource('locacao', 'LocacaoController');
 Route::apiResource('marca', 'MarcaController');
 Route::apiResource('modelo', 'ModeloController');
 
+Route::post('login', 'AuthController@login');
+Route::post('logout', 'AuthController@logout');
+Route::post('refresh', 'AuthController@refresh');
+Route::post('me', 'AuthController@me');
+
 
